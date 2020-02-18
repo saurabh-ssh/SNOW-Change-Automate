15b4724e96e6e23d9166d50ccdf1863b591a4c82
Saurabh H <saurabhhingmire007@gmail.com>
2020-02-19 00:07:39 +0530

    Create SNOW_Variables.py

---
+++ SNOW_Variables.py
@@ -0,0 +1,47 @@
+# coding: utf-8
+requestor_name="Saurabh Hingmire(with userid)"
+RITM="122345"
+agrp="Full-Group-name"
+cgrp="Full-Group-name"
+ccat="Network"
+config_item="Applicable config item"
+imple="Saurabh Hingmire(with userid) "
+short_des='''Short Description of change'''
+description=''' Description of change '''
+
+############### DIP ##############
+business_driver=''' Business Driver '''
+
+DIP=''' 
+Detail implementations Steps
+They can be multiline
+'''
+
+final_validation=''' 
+Final Validation
+'''
+
+#By Default No lower environment box is set to click. It can be changed
+pre_imp_testing='''
+pre implementation testing
+'''
+pot_cust_imp=''' Potential Customer impact'''
+risk_miti='''Risk Mitigation'''
+comm_plan='''
+Communication Plan  	
+'''
+leader="Leader with Name and ed-id in bracket e.g Saurabh Hingmire(ED123456)"
+capacity='''Capacity'''
+
+## BACKOUT ###
+#Backout duration is set to 30 mins by default. It can be changed.
+#Required testing is set to no by default. It can be changed.
+back_out= '''
+Backout plan
+'''
+
+back_out_impact='''Backout Impact'''
+
+back_out_val='''
+Backout Validation
+'''
